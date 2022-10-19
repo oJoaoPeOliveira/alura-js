7845b4af9bb214b967b9dcdc7b81bf66f0de686e
João Pedro de Oliveira <joaopedro_o@outlook.com>
2022-10-18 23:18:28 -0300

    vídeo sobre o tipo primitivo boolean

---
+++ boolean.js
@@ -0,0 +1,12 @@
+const primeiroNumero = 7;
+const segundoNumero = 17;
+
+let cadastroAtivado = false;
+
+// Comparação de valor == | tipo e valor ===
+// console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
