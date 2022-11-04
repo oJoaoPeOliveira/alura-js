9c67d31881545f94049eab9f35fe12bf8f757c33
João Pedro de Oliveira <joaopedro_o@outlook.com>
2022-11-03 21:59:42 -0300

    Informações sobre o JavaScript e o nodeJS

--- javaScriptNodeJs/node.js
+++ javaScriptNodeJs/node.js
@@ -1,1 +1,28 @@
 // Início da parte 03 do curso de tipos, variáveis e funções
+
+// Tipagem dinâmica | untyped -> Quando o tipo não precisa ser declarado
+// Ele aceita a troca de dado, e ele identifica ao receber o valor de que tipo é
+
+let minhaVar = 567;
+minhaVar = "texto";
+
+// Multiparadigma -> Um problema pode ser solucionado de várias formas
+// Sobre o nome -> era para ser EcmaScript <- Nome da fundação que cuida
+
+console.log("Código basicamente só de informações");
+
+// O que é ES6 / ES2015? -> Versão marcante que saiu em 2015 <- versão mais famosa
+// Surgiu versões anuais depois disso, pois trouxe várias funcionalidades
+// Let e const vieram com essa versão
+
+console.log("JavaScript é uma linguagem interpretada");
+
+// Escrevo o código e ele é executado por um programa, traduzindo cada linha quando
+// Estiver executando
+
+// E oque é nodeJs -> é o JavaScript -> O node é o interpretador para o back-end 
+// server side - e trás umas funções importante como do front. ex: console
+// Ferramenta desenvolvida em 2009
+
+// O navegador tem ferramentas que o node não tem, tudo que precisamos para se conectar
+// com a tela do navegador não está presente no node. O node precisa ser instalado.
