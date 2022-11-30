154f5de12266e437621ed49c7ba90bf80d2ae069
João Pedro de Oliveira <joaopedro_o@outlook.com>
2022-11-29 21:55:53 -0300

    Concatenando duas listas com concat

---
+++ array/juntandoSalas.js
@@ -0,0 +1,9 @@
+// Método para concatenar dois arrays
+
+const salaJS = ['João', 'Pedro'];
+const salaPython = ['Lara', 'Mello'];
+
+// Ele não altera nenhum dos dois, retorna um novo
+const salasUnificadas = salaJS.concat(salaPython);
+
+console.log(salasUnificadas);
